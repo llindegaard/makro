56c9820f1302e3e68fa9adf10f844a23fdb03d34
Lasse <l_lindegaard@yahoo.com>
2021-01-25 16:31:26 +0100

    Tilføjet tabel kilde

--- NKN1.R
+++ NKN1.R
@@ -21,62 +21,53 @@ NKN1.vaekst$Kvartal <- as.Date(as.yearqtr(gsub("K", "Q",NKN1.vaekst$TID)))
 
 graftype <- c("Samlet antal beskæftigede (1000 personer)" )
 prisenhed <- c("Løbende priser, (mia. kr.)")
-ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
+figur <- ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
                          & NKN1.vaekst$PRISENHED == prisenhed), ]
        , aes(x = Kvartal, y = INDHOLD)) + 
   geom_line()  + theme(legend.position = "bottom" ) +
-  labs( title = graftype, subtitle = prisenhed  , caption = "DSt")  + 
+  labs( title = graftype, subtitle = prisenhed  , caption = paste("DSt", dsttabel))  + 
   scale_x_date(date_breaks = "12 month", date_labels =  "%y") + 
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
+png(filename = paste(graftype,prisenhed,".png"), height = 800, width = 1200)
+figur
+dev.off()
 
 graftype <- c("Samlede præsterede timer (mio. timer)" )
 prisenhed <- c("Løbende priser, (mia. kr.)")
-ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
+figur <- ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
                          & NKN1.vaekst$PRISENHED == prisenhed), ]
   , aes(x = Kvartal, y = INDHOLD)) + 
   geom_line()  + theme(legend.position = "bottom" ) +
-  labs( title = graftype, subtitle = prisenhed  , caption = "DSt")  + 
+  labs( title = graftype, subtitle = prisenhed  , caption = paste("DSt", dsttabel))  + 
   scale_x_date(date_breaks = "12 month", date_labels =  "%y") + 
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
+png(filename = paste(graftype,prisenhed,".png"), height = 800, width = 1200)
+figur
+dev.off()
 
 graftype <- c("P.31 Privatforbrug")
 prisenhed <- c("2010-priser, kædede værdier, (mia. kr.)")
-ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
+figur <- ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
                          & NKN1.vaekst$PRISENHED == prisenhed), ]
        , aes(x = Kvartal, y = INDHOLD)) + 
   geom_line()  + theme(legend.position = "bottom" ) +
-  labs( title = graftype, subtitle = prisenhed  , caption = "DSt") + 
-  scale_x_date(date_breaks = "12 month", date_labels =  "%y") + 
-  theme(axis.text.x=element_text(angle = 60, hjust = 1))
-
-graftype <- c("P.31 Privatforbrug" )
-prisenhed <- c("Realvækst i forhold til foregående periode (pct.)")
-ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
-                         & NKN1.vaekst$PRISENHED == prisenhed), ]
-       , aes(x = Kvartal, y = INDHOLD)) + 
-  geom_line()  + theme(legend.position = "bottom" ) +
-  labs( title = graftype, subtitle = prisenhed  , caption = "DSt") + 
+  labs( title = graftype, subtitle = prisenhed  , caption = paste("DSt", dsttabel)) + 
   scale_x_date(date_breaks = "12 month", date_labels =  "%y") + 
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
+png(filename = paste(graftype,prisenhed,".png"), height = 800, width = 1200)
+figur
+dev.off()
 
-
-
-
-# graftype <- c("D.1 Compensation of employees" )
-# ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
-#                        & (NKN1.vaekst$SEKTOR == "S.1 Total economy"  
-#                        | NKN1.vaekst$SEKTOR == "S.11+S.12+S.14+S.15 Total economy, except General Government"
-#                  )), ]
-#        , aes(x= TID, y = pct_change, color = SEKTOR)) + 
-#   geom_line()  + theme(legend.position = "bottom", title = element_text(graftype) ) +
-#     ggtitle ( graftype  )
-# 
-# graftype <- c("B.1GF Gross domestic product at factor cost" )
-# ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
-#                        & (NKN1.vaekst$SEKTOR == "S.1 Total economy"  
-#                           | NKN1.vaekst$SEKTOR == "S.11+S.12+S.14+S.15 Total economy, except General Government"
-#                        )), ]
-#        , aes(x= TID, y = pct_change, color = SEKTOR)) + 
-#   geom_line()  + theme(legend.position = "bottom", title = element_text(graftype) ) +
-#   ggtitle ( graftype  )
+graftype <- c("P.31 Privatforbrug")
+prisenhed <- c("Realvækst i forhold til foregående periode (pct.)")
+figur <- ggplot(NKN1.vaekst[which(NKN1.vaekst$TRANSAKT == graftype 
+                         & NKN1.vaekst$PRISENHED == prisenhed), ]
+       , aes(x = Kvartal, y = INDHOLD)) + 
+  geom_line()  + theme(legend.position = "bottom" ) +
+  labs( title = graftype, subtitle = prisenhed  , caption = paste("DSt", dsttabel)) + 
+  scale_x_date(date_breaks = "12 month", date_labels =  "%y") + 
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+png(filename = paste(graftype,prisenhed,".png"), height = 800, width = 1200)
+figur
+dev.off()
 
