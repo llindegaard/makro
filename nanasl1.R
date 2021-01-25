d07315a35bdbf029b6415fc43f78d5f42b82cfdc
Lasse <l_lindegaard@yahoo.com>
2021-01-25 16:26:28 +0100

    ryddet op og gjort dansk

--- nanasl1.R
+++ nanasl1.R
@@ -1,41 +1,38 @@
 library(statsDK)
 library(tidyverse)
-nanasl1 <- sdk_retrieve_data("NASL1")
+dsttabel <- c("NASL1")
+nanasl1 <- sdk_retrieve_data(dsttabel, lang = "dk")
 
-
-nanasl1 %>% 
-  mutate(INDHOLD = str_replace(INDHOLD, ".", "NA"))
 nanasl1$INDHOLD <- as.numeric(nanasl1$INDHOLD)
-<<<<<<< HEAD
-# vv
-=======
 
->>>>>>> 9f62c77b0a714376423e42bfecdeb993eeb12c55
-
-l1.vaekst <- nanasl1 %>%
+nanasl1.vaekst <- nanasl1 %>%
   group_by(TRANSAKT, SEKTOR) %>% 
   arrange(c(TID), .by_group = TRUE) %>%
   mutate(pct_change = (INDHOLD/lag(INDHOLD) - 1) * 100) %>%
-  # filter(TRANSAKT == "D.1 Compensation of employees" & SEKTOR == "S.11+S.12+S.14+S.15 Total economy, except General Government") %>%
   filter(TID > 1989)
- 
-distinct(l1.vaekst$TRANSAKT)
-l1.vaekst <- as.data.frame(l1.vaekst)
 
-graftype <- c("D.1 Compensation of employees" )
-ggplot(l1.vaekst[which(l1.vaekst$TRANSAKT == graftype 
-                       & (l1.vaekst$SEKTOR == "S.1 Total economy"  
-                       | l1.vaekst$SEKTOR == "S.11+S.12+S.14+S.15 Total economy, except General Government"
+graftype <- c("D.1 Aflønning af ansatte" )
+prisenhed <- c("Aflønning")
+figur <- ggplot(nanasl1.vaekst[which(nanasl1.vaekst$TRANSAKT == graftype 
+                       & (nanasl1.vaekst$SEKTOR == "S.1 Hele økonomien"  
+                       | nanasl1.vaekst$SEKTOR == "S.11+S.12+S.14+S.15 Selskaber, organisationer og husholdninger"
                  )), ]
-       , aes(x= TID, y = pct_change, color = SEKTOR)) + 
+       , aes(x = TID, y = pct_change, color = SEKTOR)) + 
   geom_line()  + theme(legend.position = "bottom", title = element_text(graftype) ) +
-    ggtitle ( graftype  )
+  labs( title = graftype, subtitle = prisenhed  , caption = paste("DSt", dsttabel)  )
+png(filename = paste(graftype,prisenhed,".png"), height = 800, width = 1200)
+figur
+dev.off()
 
-graftype <- c("B.1GF Gross domestic product at factor cost" )
-ggplot(l1.vaekst[which(l1.vaekst$TRANSAKT == graftype 
-                       & (l1.vaekst$SEKTOR == "S.1 Total economy"  
-                          | l1.vaekst$SEKTOR == "S.11+S.12+S.14+S.15 Total economy, except General Government"
+graftype <- c("B.1GF Bruttofaktorindkomst, BFI" )
+prisenhed <- c("Faktoromkostninger")
+figur <- ggplot(nanasl1.vaekst[which(nanasl1.vaekst$TRANSAKT == graftype 
+                       & (nanasl1.vaekst$SEKTOR == "S.1 Hele økonomien"  
+                          | nanasl1.vaekst$SEKTOR == "S.11+S.12+S.14+S.15 Selskaber, organisationer og husholdninger"
                        )), ]
-       , aes(x= TID, y = pct_change, color = SEKTOR)) + 
+       , aes(x = TID, y = pct_change, color = SEKTOR)) + 
   geom_line()  + theme(legend.position = "bottom", title = element_text(graftype) ) +
-  ggtitle ( graftype  )
+  labs( title = graftype, subtitle = prisenhed  , caption = "DSt") 
+png(filename = paste(graftype,prisenhed,".png"), height = 800, width = 1200)
+figur
+dev.off()
